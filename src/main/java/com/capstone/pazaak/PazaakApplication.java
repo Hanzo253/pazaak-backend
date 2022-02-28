5cf37a9a4f82efd6267287a77d3b2bcd2868706d
David Doan <daviddoan45@gmail.com>
2022-02-28 17:14:43 -0500

    generated pazaak-backend springboot app

---
+++ src/main/java/com/capstone/pazaak/PazaakApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.pazaak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PazaakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PazaakApplication.class, args);
+    }
+
+}
