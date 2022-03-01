3b0250e7ca8044669362784af81c8bc599c6e5ce
David Doan <daviddoan45@gmail.com>
2022-03-01 11:19:43 -0500

    finished json web token classes

--- src/main/java/com/capstone/pazaak/security/SecurityConfigurer.java
+++ src/main/java/com/capstone/pazaak/security/SecurityConfigurer.java
@@ -1,4 +1,14 @@
 package com.capstone.pazaak.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 public class SecurityConfigurer {
+    private  MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
 }
