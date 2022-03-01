a4cda806996d6186c993021f1ba34776a95cda52
David Doan <daviddoan45@gmail.com>
2022-03-01 00:31:04 -0500

    finished UserRepository

--- src/main/java/com/capstone/pazaak/model/User.java
+++ src/main/java/com/capstone/pazaak/model/User.java
@@ -1,4 +1,18 @@
 package com.capstone.pazaak.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private Long id;
+
+    @Column
+    private String userName;
+
+    @Column(unique = true)
+    private String emailAddress;
 }
--- src/main/java/com/capstone/pazaak/repository/UserRepository.java
+++ src/main/java/com/capstone/pazaak/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package com.capstone.pazaak.repository;
 
-public class UserRepository {
+import com.capstone.pazaak.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository extends JpaRepository<User, Long> {
+    // to register
+    boolean existsByEmailAddress(String userEmailAddress);
+
+    // to login
+    User findUserByEmailAddress(String userEmailAddress);
+
+    // get username
+    User findUserByUserName(String username);
 }
