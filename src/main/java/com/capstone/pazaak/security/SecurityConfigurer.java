d9260a899f15b8bef8bb04a0035d8b62da3c20a6
David Doan <daviddoan45@gmail.com>
2022-03-01 11:26:55 -0500

    finished security package classes

--- src/main/java/com/capstone/pazaak/security/SecurityConfigurer.java
+++ src/main/java/com/capstone/pazaak/security/SecurityConfigurer.java
@@ -1,7 +1,20 @@
 package com.capstone.pazaak.security;
 
+import com.capstone.pazaak.security.jwt.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.context.WebApplicationContext;
 
 @EnableWebSecurity
 public class SecurityConfigurer {
@@ -10,5 +23,50 @@ public class SecurityConfigurer {
     @Autowired
     public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
         this.myUserDetailsService = myUserDetailsService;
+    }
+
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public void setJwtRequestFilter(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    // step 1
+
+    /**
+     * We use the PasswordEncoder that is defined in the Spring Security configuration to encode the password. * @return
+     */
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // step 2
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // only allowed urls with out JWT
+        http.authorizeRequests().antMatchers("/auth/users", "/auth/users/login", "/auth/users/register", "/auth/users/list", "/auth/users/{username}", "/auth/users/delete", "/auth/users/changepassword").permitAll()
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    // fetching data for user for authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public MyUserDetails myUserDetails() {
+        return (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
